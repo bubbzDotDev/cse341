75dad448cf16525428f0d7dff60889d40a98c63e
bubbzDotDev <christopher.blount@outlook.com>
2022-01-06 20:43:15 -0600

    finished section 2

---
+++ section02/play.js
@@ -0,0 +1,15 @@
+const fetchData = () => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!');
+    });
+  });
+  return promise;
+};
+
+setTimeout(() => {
+  console.log('Timer is done!');
+  fetchData().then(text => {
+    console.log(text);
+  });
+}, 2000);
