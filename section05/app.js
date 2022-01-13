bf13f090c226e7af78d7c558cf182ef9f2594f7a
bubbzDotDev <christopher.blount@outlook.com>
2022-01-13 11:19:36 -0600

    almost done with section 5

--- section05/app.js
+++ section05/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<h1>Add Product Page</h1>');
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express</h1>');
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
---
+++ section05/routes/admin.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const router = express.Router();
+
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+});
+
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+module.exports = router;
