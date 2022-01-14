aefdf5556ed40b1d4da08348340ea1f60d72e660
bubbzDotDev <christopher.blount@outlook.com>
2022-01-14 01:17:35 -0600

    almost done with section 6

---
+++ section06/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', {
+    pageTitle: 'Page Not Found'
+  });
+});
+
+app.listen(3000);
---
+++ section06/routes/admin.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+const products = [];
+
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product'
+  });
+});
+
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
