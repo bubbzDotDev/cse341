3f9e432ab7c8646ee10ee70ad2970241b390e29a
bubbzDotDev <christopher.blount@outlook.com>
2022-01-28 21:06:49 -0600

    Finished section 11

--- section11/controllers/shop.js
+++ section11/controllers/shop.js
@@ -142,8 +142,13 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
+  req.user.getOrders({ include: ['products'] })
+    .then(orders => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders
+      });
+    })
+      .catch(err => console.log(err));
 };
