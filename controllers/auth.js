acc8c77d0e1d2b2815f9e7353b893080cb92315c
bubbzDotDev <christopher.blount@outlook.com>
2022-02-03 23:55:01 -0600

    Section 15 starting setup

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,14 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
+
 exports.postLogin = (req, res, next) => {
   User.findById('61f4db462760174d2486bca0')
     .then(user => {
@@ -20,6 +28,8 @@ exports.postLogin = (req, res, next) => {
     })
     .catch(err => console.log(err));
 };
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
--- controllers/error.js
+++ controllers/error.js
@@ -1,6 +1,6 @@
 exports.get404 = (req, res, next) => {
-  res.status(404).render('404', { 
-    pageTitle: 'Page Not Found', 
+  res.status(404).render('404', {
+    pageTitle: 'Page Not Found',
     path: '/404',
     isAuthenticated: req.session.isLoggedIn
   });
